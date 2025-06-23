16ce02e899c7a112ef35ede70bfd9c295d094d74
pedro <pedrogscarlassara@gmail.com>
2025-06-23 13:49:47 -0300

    refactor: added option 8 (todo)

--- s3_basics.py
+++ s3_basics.py
@@ -3,93 +3,124 @@ from botocore.exceptions import NoCredentialsError, ParamValidationError
 import requests
 
 session = boto3.Session(
-    profile_name = 'Scarlassara',
-    region_name = 'us-east-1'
+    profile_name='Scarlassara',
+    region_name='us-east-1'
 )
 
 client = session.client('s3')
 
+
 def check_internet_connection():
     response = requests.get('https://checkip.amazonaws.com/')
-    if response.status_code == 200:
-        return True
-    else:
-        return False
+    return response.status_code == 200
+
 
 def main():
     if check_internet_connection():
-        print('1. List Bucket          2. Create Bucket            3. Delete Bucket \n'
-              '4. Upload Object        5. Downlaod Object            6. Delete Object')
+        print(
+            '1. List Buckets         2. Create Bucket        3. Delete Bucket\n'
+            '4. Create Object        5. Upload Object        6. Download Object\n'
+            '7. Delete Object        8. TODO'
+        )
 
-        option = int(input())
+        option = int(input("Select an option: "))
 
         if option == 1:
             print(client.list_buckets()['Buckets'])
+
         elif option == 2:
             try:
                 bucket_name = input('Name your bucket: ')
-                bucket_region = input('Which region do you want to create the bucket: ')
+                bucket_region = input('Which region do you want to create the bucket in? ')
                 response = client.create_bucket(
-                    Bucket= f'{bucket_name}',
-                    #Check the avaiable regions at: https://docs.aws.amazon.com/AmazonS3/latest/API/API_CreateBucketConfiguration.html
+                    Bucket=bucket_name,
                     CreateBucketConfiguration={
-                        'LocationConstraint': f'{bucket_region}',
+                        'LocationConstraint': bucket_region,
                     },
                 )
                 print(response)
             except client.exceptions.BucketAlreadyExists:
-                print('This bucket already exist.')
+                print('This bucket already exists.')
             except client.exceptions.ClientError as error:
                 print(f'ClientError: {error}')
+
         elif option == 3:
-            bucket_name = str(input('Bucket Name: '))
+            bucket_name = input('Bucket Name: ')
             try:
-                response  = client.delete_bucket(Bucket=str(bucket_name))
+                response = client.delete_bucket(Bucket=bucket_name)
                 print(response)
             except ParamValidationError as error:
                 print(f'ParamValidationError: {error}')
             except client.exceptions.ClientError as error:
-                print(f'Client Error: {error}')
+                print(f'ClientError: {error}')
+
         elif option == 4:
             bucket_name = input('Bucket name: ')
-            object_name = input('Object content: ')
+            object_content = input('Object content: ')
             object_key = input('Object name: ')
             response = client.put_object(
                 ACL='private',
                 Bucket=bucket_name,
-                Body=object_name,
+                Body=object_content,
                 Key=object_key,
                 ServerSideEncryption='AES256',
                 StorageClass='STANDARD'
             )
             print(response)
+
         elif option == 5:
             try:
                 bucket_name = input('Bucket Name: ')
-                object_name = input('Object Name: ')
-                file_name = input('File Name: ')
-                response = client.download_file(
+                file_path = input('File Path to Upload: ')
+                object_name = input('Object Name on S3: ')
+                response = client.upload_file(
+                    file_path,
                     bucket_name,
-                    object_name,
-                    file_name
-
+                    object_name
                 )
-                print(response)
+                print('Upload completed successfully.')
             except PermissionError as error:
                 print(f'PermissionError: {error}')
-
-
-
+            except FileNotFoundError as error:
+                print(f'FileNotFoundError: {error}')
+            except client.exceptions.ClientError as error:
+                print(f'ClientError: {error}')
 
         elif option == 6:
-            object_name = input('Insert the object name: ')
-            bucket_name = input('Bucket nme: ')
+            try:
+                bucket_name = input('Bucket Name: ')
+                object_name = input('Object Name on S3: ')
+                destination_file = input('Destination File Name: ')
+                client.download_file(
+                    bucket_name,
+                    object_name,
+                    destination_file
+                )
+                print('Download completed successfully.')
+            except PermissionError as error:
+                print(f'PermissionError: {error}')
+            except FileNotFoundError as error:
+                print(f'FileNotFoundError: {error}')
+            except client.exceptions.ClientError as error:
+                print(f'ClientError: {error}')
+
+        elif option == 7:
+            object_name = input('Object Name: ')
+            bucket_name = input('Bucket Name: ')
             response = client.delete_object(
                 Bucket=bucket_name,
                 Key=object_name
             )
             print(response)
+
+        elif option == 8:
+            print('TODO: Implement this option.')
+
+        else:
+            print("Invalid option.")
+
+
 try:
     main()
 except ValueError:
-    print('Invalid option.')
+    print('Invalid input.')
